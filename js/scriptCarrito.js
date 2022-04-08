d9f665b8920154d4c0d95c49e732917145f3c3a7
Camila Coppola <coppolacamilaa@gmail.com>
2022-04-08 15:26:03 -0300

    Entrega Final + Coppola Camila

--- js/scriptCarrito.js
+++ js/scriptCarrito.js
@@ -1,71 +1,33 @@
+/* 
+==============================================
+TODO LO REFERENTE A LA PÁGINA CARRITO.HTML
+==============================================
+*/
+
 // Variables
-const listaProductos = [
-    {
-        id: 1,
-        nombre: 'Optica Chevrolet Cruze 2016 en adelante -Original-',
-        precio: 15500,
-        imagen: 'assets/opticaCruze.jpg'
-    },
-    {
-        id: 2,
-        nombre: 'Faro trasero color blanco Peugeot 208 -Original-',
-        precio: 10200,
-        imagen: 'assets/faro208.jpeg'
-    },
-    {
-        id: 3,
-        nombre: 'Optica delantera Honda Fit 2012/2016 -Original-',
-        precio: 10900,
-        imagen: 'assets/opticaHonda.jpeg'
-    },
-    {
-        id: 4,
-        nombre: 'Optica izquierda Focus 2015/2020 -Original-',
-        precio: 20100,
-        imagen: 'assets/opticaFocus.jpeg'
-    },
-    {
-        id: 5,
-        nombre: 'Optica Alma delantera Ford Ka 2008/2013 -Original-',
-        precio: 14500,
-        imagen: 'assets/almaKa.jpeg'
-    },
-    {
-        id: 6,
-        nombre: 'Optica derecha Palio F.II F/Cromado A/cromado -Original-',
-        precio: 11690,
-        imagen: 'assets/opticaPalio.jpeg'
-    },
-    {
-        id: 7,
-        nombre: 'Optica Panel trasero interior Renault Logan -Original-',
-        precio: 9900,
-        imagen: 'assets/panelLogan.jpeg'
-    },
-    {
-        id: 8,
-        nombre: 'Panel cola trasera Honda Civic Linea 2006/2011 -Original-',
-        precio: 25600,
-        imagen: 'assets/colaHonda.jpeg'
-    }
-   
-
-];
-
 let carrito = [];
 const moneda = '$';
 const DOMitems = document.querySelector('#items');
 const DOMcarrito = document.querySelector('#carrito');
 const DOMtotal = document.querySelector('#total');
 const DOMbotonVaciar = document.querySelector('#boton-vaciar');
+const DOMbotonComprar = document.querySelector('#boton-comprar');
+const miLocalStorage = window.localStorage
+
+
+let listaProductos = [];
+fetch('../data.json')
+.then((resp) => resp.json())
+.then((data) => renderizarProductos(data))
 
 // Funciones
 
 /**
  * Crea todos los productos, apartir de la lista de productos. 
  */
-function renderizarProductos() {
-     listaProductos.forEach((info) => {
+function renderizarProductos(productos) {
+    console.log(productos)
+      productos.forEach((info) => {
         // Se creo la Estructura - "Card"
         const miNodo = document.createElement('div');
         miNodo.classList.add('card', 'col-sm-4');
@@ -89,7 +51,7 @@ function renderizarProductos() {
         miNodoBoton.classList.add('btn', 'btn-primary');
         miNodoBoton.textContent = 'Comprar';
         miNodoBoton.setAttribute('marcador', info.id);
-        miNodoBoton.addEventListener('click', aniadirProductoAlCarrito);
+        miNodoBoton.addEventListener('click', function(){aniadirProductoAlCarrito(event,productos)});
         // Implementacion
         miNodoCardBody.appendChild(miNodoImagen);
         miNodoCardBody.appendChild(miNodoTitle);
@@ -103,18 +65,34 @@ function renderizarProductos() {
 /**
  * Evento para poder añadir un producto al carrito de la compra
  */
-function aniadirProductoAlCarrito(evento) {
+function aniadirProductoAlCarrito(evento,productos) {
+    Toastify({
+        text: "Agregaste el producto al carrito",
+        duration: 3000,
+        destination: "https://github.com/apvarun/toastify-js",
+        newWindow: true,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+          background: "#B2AAFD",
+          color: "#242424"
+        },
+        onClick: function(){} // Callback after click
+      }).showToast();
     // Aniadimos el Nodo a nuestro carrito
     carrito.push(evento.target.getAttribute('marcador'))
     // Actualizamos el carrito 
-    renderizarCarrito();
+    renderizarCarrito(productos);
+    guardarCarritoEnLocalStorage(productos);
 
 }
 
 /**
  * Muestra los productos guardados en el carrito
  */
-function renderizarCarrito() {
+function renderizarCarrito(productos) {
     // Vaciamos todo el html
     DOMcarrito.textContent = '';
     // Quitamos los productos duplicados
@@ -122,7 +100,7 @@ function renderizarCarrito() {
     // Generamos los Nodos a partir de carrito
     carritoSinDuplicados.forEach((item) => {
         // Obtenemos el item que necesitamos de la variable base de datos
-        const miItem = listaProductos.filter((itemListaProductos) => {
+        const miItem = productos.filter((itemListaProductos) => {
             // ¿Coincide las id? Solo puede existir un caso
             return itemListaProductos.id === parseInt(item);
         });
@@ -141,37 +119,47 @@ function renderizarCarrito() {
         miBoton.textContent = 'X';
         miBoton.style.marginLeft = '1rem';
         miBoton.dataset.item = item;
-        miBoton.addEventListener('click', borrarItemCarrito);
+        miBoton.addEventListener('click', function(){borrarItemCarrito(event,productos)} );
         // Mezclamos nodos
         miNodo.appendChild(miBoton);
         DOMcarrito.appendChild(miNodo);
     });
     // Renderizamos el precio total en el HTML
-    DOMtotal.textContent = calcularTotal();
+    DOMtotal.textContent = calcularTotal(productos);
 }
 
 /**
  * Evento para borrar un elemento del carrito
  */
-function borrarItemCarrito(evento) {
+function borrarItemCarrito(evento,productos) {
     // Obtenemos el producto ID que hay en el boton pulsado
     const id = evento.target.dataset.item;
     // Borramos todos los productos
     carrito = carrito.filter((carritoId) => {
         return carritoId !== id;
     });
-    // volvemos a renderizar
-    renderizarCarrito();
+    Toastify({
+        text: "Eliminaste el producto de tu carrito",
+        className: "info",
+        style: {
+            background: "#B2AAFD",
+            color: "#242424"
+        }
+      }).showToast();
+
+    renderizarCarrito(productos);
+    guardarCarritoEnLocalStorage(productos);
+
 }
 
 /**
  * Calcula el precio total teniendo en cuenta los productos repetidos
  */
-function calcularTotal() {
+function calcularTotal(productos) {
     // Recorremos el array del carrito 
     return carrito.reduce((total, item) => {
         // De cada elemento obtenemos su precio
-        const miItem = listaProductos.filter((itemBaseDatos) => {
+        const miItem = productos.filter((itemBaseDatos) => {
             return itemBaseDatos.id === parseInt(item);
         });
         // Los sumamos al total
@@ -180,18 +168,76 @@ function calcularTotal() {
 }
 
 /**
- * Varia el carrito y vuelve a dibujarlo
+ * Vacia el carrito y vuelve a dibujarlo
  */
 function vaciarCarrito() {
-    // Limpiamos los productos guardados
-    carrito = [];
-    // Renderizamos los cambios
-    renderizarCarrito();
+    const swalWithBootstrapButtons = Swal.mixin({
+        customClass: {
+          confirmButton: 'btn btn-success',
+          cancelButton: 'btn btn-danger'
+        },
+        buttonsStyling: false
+      })
+      swalWithBootstrapButtons.fire({
+        title: '¿Desea vaciar el carrito de compra?',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Si, vaciar carrito.',
+        cancelButtonText: 'No, cancelar',
+        reverseButtons: true
+      }).then((result) => {
+        if (result.isConfirmed) {
+            carrito = [];
+          swalWithBootstrapButtons.fire(
+            'Se ha borrado con exito!',
+            'Los productos del carrito fueron eliminados.',
+          )
+          renderizarCarrito();
+          localStorage.clear();
+
+        } else if (
+          /* Read more about handling dismissals below */
+          result.dismiss === Swal.DismissReason.cancel
+        ) {
+          swalWithBootstrapButtons.fire(
+            'Proceso cancelado',
+            'Los productos de tu carrito no fueron eliminados',
+          )
+        }
+      })
 }
+/*Evento para comprar elementos del carrito*/
+function comprarCarrito() {
+  carrito = [];
+  renderizarCarrito();
+  localStorage.clear();
+
+  Swal.fire({
+      position: 'top-center',
+      icon: 'success',
+      title: '¡Compra realizada con exito!.',
+      showConfirmButton: false,
+      timer: 1500
+  })
+}
+
+function guardarCarritoEnLocalStorage() {
+  miLocalStorage.setItem("carrito", JSON.stringify(carrito));
+}
+
+function cargarCarritoDeLocalStorage() {
+  if (miLocalStorage.getItem('carrito') !== null) {
+      carrito = JSON.parse(miLocalStorage.getItem('carrito'));
+  }
+}
+
+const domBotonVaciar = document.querySelector("#boton-vaciar");
+const domBotonComprar = document.querySelector("#boton-comprar");
 
 // Eventos
 DOMbotonVaciar.addEventListener('click', vaciarCarrito);
+DOMbotonComprar.addEventListener("click", comprarCarrito);
 
-// Inicio
-renderizarProductos();
-renderizarCarrito();
+//Inicio
+cargarCarritoDeLocalStorage();
+
